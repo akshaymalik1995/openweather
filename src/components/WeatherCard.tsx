93ba966b2d43a8412e3b563d0a8f0ed535162454
akshaymalik1995 <akshaymalik191@gmail.com>
2024-07-09 17:42:57 +0530

    WeatheCard UI Changes

--- src/components/WeatherCard.tsx
+++ src/components/WeatherCard.tsx
@@ -24,7 +24,7 @@ export default function WeatherCard(props: { weatherData: any }) {
   return (
     <div className="">
       <div className="">
-        <h2 className="text-6xl text-center my-12 dark:text-white">
+        <h2 className="text-3xl md:text-6xl text-center my-16 dark:text-white">
           {weatherData.location}, {weatherData.country}
         </h2>
         <div className="mx-auto bg-opacity-60 flex flex-col max-w-2xl  dark:bg-gray-800 shadow-lg w-full rounded-lg dark:text-gray-100">
@@ -34,10 +34,10 @@ export default function WeatherCard(props: { weatherData: any }) {
               {weatherData.timezone && getCityTime(weatherData.timezone)}
             </div>
           </div>
-          <div className="grid items-center px-4 py-4 grid-cols-2">
-            <div className="flex items-center gap-4  col-span-1">
+          <div className="grid items-center grid-cols-1 md:grid-cols-2">
+            <div className="flex p-4 items-center justify-center gap-4  col-span-1">
               <div className="dark:text-white flex flex-col  gap-2  px-2 rounded p-1 ">
-                <div className="flex gap-2 items-center ">
+                <div className="flex  gap-2 items-center ">
                   {weatherData.weather in weatherIconMapping ? (
                     <div className="text-6xl">
                       {weatherIconMapping[weatherData.weather]}
@@ -50,10 +50,10 @@ export default function WeatherCard(props: { weatherData: any }) {
                       {weatherData.temperature &&
                         weatherData.temperature.toFixed()}
                     </div>
-                    <div className="absolute -right-3 md:-right-8  font-bold -top-1 md:-top-2 text-3xl md:text-5xl">
+                    <div className="absolute -right-3 md:-right-8  font-bold -top-0 md:-top-2 text-2xl md:text-5xl">
                       o
                     </div>
-                    <div className="absolute -right-4 md:-right-7 text-gray-400 font-bold bottom-2 text-3xl  md:text-5xl">
+                    <div className="absolute bottom-1 -right-3 md:-right-7 text-gray-400 font-bold md:bottom-2 text-3xl  md:text-5xl">
                       c
                     </div>
                   </div>
@@ -64,7 +64,7 @@ export default function WeatherCard(props: { weatherData: any }) {
                 </div>
               </div>
             </div>
-            <div className="flex col-span-1  flex-col">
+            <div className="flex dark:bg-gray-600 bg-gray-200 col-span-1 justify-center p-4 h-full  flex-col">
               <div className=" px-4 p-2 font-semibold flex justify-between ">
                 <span>Feels Like</span>{" "}
                 <span>
